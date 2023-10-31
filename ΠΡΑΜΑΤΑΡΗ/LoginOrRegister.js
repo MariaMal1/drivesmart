eec06aaea6ad6444baf33d3f45fba3efb7f78cb8
Maria <mariamaleskou7@gmail.com>
2023-10-31 17:07:11 +0200

    test

--- ΠΡΑΜΑΤΑΡΗ/LoginOrRegister.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("login-toggle").addEventListener("click", function() {
-    document.getElementById("login-form").style.display = "block";
-    document.getElementById("register-form").style.display = "none";
-});
-
-document.getElementById("register-toggle").addEventListener("click", function() {
-    document.getElementById("login-form").style.display = "none";
-    document.getElementById("register-form").style.display = "block";
-});
